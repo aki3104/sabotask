8da7cc95650503e2982121e4f35c8b965b28ae1b
3no3 <ulis7524@gmail.com>
2020-02-22 11:51:23 +0900

    align indent

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :tasks
-    validates :name, presence: true
-    validates :email, presence: true
-    validates :password, presence: true
+    validates :name,        presence: true
+    validates :email,       presence: true
+    validates :password,    presence: true
 end
