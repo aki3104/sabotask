0706c36f8e03631e398eda207588d817b2feae41
terufumi1122 <terufumi1122@gmail.com>
2020-02-22 18:59:58 +0900

    create user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+
+  describe 'ファクトリのテスト' do
+    it '有効なファクトリを持つこと' do
+      expect(user).to be_valid
+    end
+  end
+
+  describe 'バリデーションのテスト' do
+    it 'nameがなければエラーになる' do
+      user = build(:user, name: nil)
+      expect(user.save).to be_falsey
+    end
+
+    it 'emailがなければエラーになる' do
+      user = build(:user, email: nil)
+      expect(user.save).to be_falsey
+    end
+
+    it 'passwordがなければエラーになる' do
+      user = build(:user, password: nil)
+      expect(user.save).to be_falsey
+    end
+  end
 end
