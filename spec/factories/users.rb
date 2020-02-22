35d2c9a77d25aa3986223cb1aeb08a10179227e4
terufumi1122 <terufumi1122@gmail.com>
2020-02-22 18:11:10 +0900

    create users factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :user do
-    
+    password = Faker::Internet.password(min_length: 8, max_length: 16)
+
+    name                  { Faker::Name.name }
+    email                 { Faker::Internet.free.email }
+    password              { password }
+    password_confirmation { password }
   end
 end
