71305b5e22e076a3ee0d94e75dc7520683497ed9
terufumi1122 <terufumi1122@gmail.com>
2020-03-22 12:09:53 +0900

    chore: dcew bundle exec rails g controller api/v1/sessions create destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'sessions/create'
+      get 'sessions/destroy'
+    end
+  end
   root to: 'home#index'
 
   namespace :api, {format: 'json'} do
