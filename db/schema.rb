88b907e2af97192a0a5ae47e72adf0ef77a94e89
3no3 <ulis7524@gmail.com>
2020-03-22 16:50:06 +0900

    モデル修正：　Goalモデルにactive_flagを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_22_053239) do
+ActiveRecord::Schema.define(version: 2020_03_22_074048) do
 
   create_table "goals", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.bigint "user_id"
     t.string "goal"
     t.integer "week"
+    t.boolean "active_flag", default: true
     t.index ["user_id"], name: "index_goals_on_user_id"
   end
 
